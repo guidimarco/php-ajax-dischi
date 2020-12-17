3e73934f0b0787d36d7f4fd39c25426d727edd89
Marco Guidi <mark.88g@gmail.com>
2020-12-17 14:37:45 +0100

    aggiunto stile base

---
+++ php/index.php
@@ -0,0 +1,33 @@
+<?php include "dischi.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>php-dischi</title>
+
+        <!-- css -->
+        <link rel="stylesheet" href="../dist/app.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png" alt="brand logo spotify">
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="album-container">
+                    <div class="album-card">
+                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+                        <h2>Title</h2>
+                        <p>Author</p>
+                        <p>years</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
