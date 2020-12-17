a5344b9b401c01bdbebe898eb4c7464db0673388
Marco Guidi <mark.88g@gmail.com>
2020-12-17 16:39:52 +0100

    fatta funzione get genre

--- dist/mainjq.js
+++ dist/mainjq.js
@@ -1,3 +1,6 @@
+// GLOBAL VAR
+let all_genre = []; // array of all-genre
+
 // handlebar album card
 const album_card_html = document.getElementById("album-card-template").innerHTML;
 const album_card_template = Handlebars.compile(album_card_html);
@@ -18,14 +21,30 @@ function print_album_card(album_array) {
         $(".album-container").append(final_album_html);
     });
 }; // print in the template
+function get_genre(album_array) {
+    let genre_array = [];
+    // console.log(album_array);
+
+    album_array.forEach((album) => {
+        // get all album.genre and push in array if not includes
+        if (!genre_array.includes(album.genre)) {
+            genre_array.push(album.genre);
+        }
+    });
+    // console.log(genre_array);
+    return genre_array;
+}; // return an array of all genre
 
 $(document).ready(function() {
-    // ajax call
+    // 1° ajax call
     $.ajax({
         url: "../jquery/dischi.php",
         method: "GET",
         success: function(dischi) {
-            print_album_card(dischi);
+            // console.log(dischi);
+            print_album_card(dischi); // print all card
+            all_genre = get_genre(dischi); // get all genre
+            console.log(all_genre);
         },
         error: function() {
             console.log("errore");
