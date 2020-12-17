ade75d47be61e26d96a809e903ba8ddbb8903e3f
Marco Guidi <mark.88g@gmail.com>
2020-12-17 15:59:48 +0100

    linkato handlebar e create card dinamiche

--- dist/mainjq.js
+++ dist/mainjq.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
+    // handlebar album card
+    const album_card_html = document.getElementById("album-card-template").innerHTML;
+    const album_card_template = Handlebars.compile(album_card_html);
+
+    // ajax call
     $.ajax({
         url: "../jquery/dischi.php",
         method: "GET",
-        success: function() {
-            console.log("ok");
+        success: function(dischi) {
+            // console.log(dischi);
+
+            // get all album-info
+            dischi.forEach((album) => {
+                // obj-info
+                var album_info = {
+                    poster: album.poster,
+                    title: album.title,
+                    author: album.author,
+                    year: album.year
+                };
+                var final_album_html = album_card_template(album_info);
+
+                // append in container
+                $(".album-container").append(final_album_html);
+            });
         },
         error: function() {
             console.log("errore");
