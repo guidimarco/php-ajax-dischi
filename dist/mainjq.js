af0025e5e7c026c5276d0359f68286b6390d0db7
Marco Guidi <mark.88g@gmail.com>
2020-12-17 16:18:08 +0100

    COMPLETATA PARTE 2 aggiunta funzione stampa in pagina

--- dist/mainjq.js
+++ dist/mainjq.js
@@ -1,32 +1,36 @@
-$(document).ready(function() {
-    // handlebar album card
-    const album_card_html = document.getElementById("album-card-template").innerHTML;
-    const album_card_template = Handlebars.compile(album_card_html);
+// handlebar album card
+const album_card_html = document.getElementById("album-card-template").innerHTML;
+const album_card_template = Handlebars.compile(album_card_html);
 
+function print_album_card(album_array) {
+    // get all album-info
+    album_array.forEach((album) => {
+        // obj-info
+        var album_info = {
+            poster: album.poster,
+            title: album.title,
+            author: album.author,
+            year: album.year
+        };
+        var final_album_html = album_card_template(album_info);
+
+        // append in container
+        $(".album-container").append(final_album_html);
+    });
+}; // print in the template
+
+$(document).ready(function() {
     // ajax call
     $.ajax({
         url: "../jquery/dischi.php",
         method: "GET",
         success: function(dischi) {
-            // console.log(dischi);
-
-            // get all album-info
-            dischi.forEach((album) => {
-                // obj-info
-                var album_info = {
-                    poster: album.poster,
-                    title: album.title,
-                    author: album.author,
-                    year: album.year
-                };
-                var final_album_html = album_card_template(album_info);
-
-                // append in container
-                $(".album-container").append(final_album_html);
-            });
+            print_album_card(dischi);
         },
         error: function() {
             console.log("errore");
         }
     })
+
+    
 });
